141e9f22728327e75cf951390d827054c803718c
Aleksandr Efremov <topefremov@yandex.ru>
2018-04-11 15:49:49 +0300

    Change authorization logic

--- src/main/java/io/github/efrem/springsecurityrest/service/HouseService.java
+++ src/main/java/io/github/efrem/springsecurityrest/service/HouseService.java
@@ -2,13 +2,13 @@ package io.github.efrem.springsecurityrest.service;
 
 import io.github.efrem.springsecurityrest.domain.House;
 import org.springframework.security.access.prepost.PostAuthorize;
-import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
 public interface HouseService {
 
-    @PostFilter("hasPermission(filterObject, 'READ')")
+    @PreAuthorize("hasPermission(#organizationId, 'io.github.efrem.springsecurityrest.domain.Organization', 'READ')")
     List<House> getHousesForOrganization(Long organizationId);
 
     @PostAuthorize("hasPermission(returnObject, 'READ')")
